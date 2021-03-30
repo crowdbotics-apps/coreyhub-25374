d5dbe4534a744e9ed4e5508e24062532b8e0ab2a
Crowdbotics <team@crowdbotics.com>
2021-03-30 23:42:13 +0000

    remove package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -3,7 +3,6 @@ export const installed_blueprints = [
 { name: 'UserProfile20220194', human_name: 'User Profile 20', access_route: 'UserProfile20220194'},
 { name: 'ArticleList220177', human_name: 'Article List', access_route: 'ArticleList220177'},
 { name: 'ArticleList220176', human_name: 'Article List', access_route: 'ArticleList220176'},
-{ name: 'ArticleList220175', human_name: 'Article List', access_route: 'ArticleList220175'},
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ]
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from "./sideMenu"
 import UserProfile20220194Navigator from '../features/UserProfile20220194/navigator';
 import ArticleList220177Navigator from '../features/ArticleList220177/navigator';
 import ArticleList220176Navigator from '../features/ArticleList220176/navigator';
-import ArticleList220175Navigator from '../features/ArticleList220175/navigator';
 
 /**
  * new navigators can be imported here
@@ -19,7 +18,6 @@ const AppNavigator = {
 UserProfile20220194: { screen: UserProfile20220194Navigator },
 ArticleList220177: { screen: ArticleList220177Navigator },
 ArticleList220176: { screen: ArticleList220176Navigator },
-ArticleList220175: { screen: ArticleList220175Navigator },
 
   /** new navigators can be added here */
   SplashScreen: {
